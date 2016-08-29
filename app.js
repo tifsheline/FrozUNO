0782bf76f659d3f7b11c43cf7df090eca7202947
Tiffany Glover <tif.sheline@gmail.com>
2016-08-28 19:06:50 -0700

    Add Pseudo code

---
+++ app.js
@@ -0,0 +1,33 @@
+//FUNCTION DECLARATIONS:
+
+
+
+//EVENT LISTENERS:
+//Event Listener for when a player clicks the Start New Game Button
+
+//Event Listener for when a player clicks the Reset Button
+
+//Event Listener for when a player clicks the End Game Button
+
+//Event Listener for when a player clicks on a card to play it
+
+
+//FUNCTIONS:
+//Function that shuffles cards
+
+//Function that places a card in the discard pile when starting a new Game
+
+//Functions for game rules
+//Players must match card in the Discard pile by either number, color, or symbol/Action. If the player has no matches or chooses not to play any of their cards they must get a card from the Draw Pile.
+
+//Function that allows players to get a card from the Draw Pile
+
+//Function for simulating flip of the cards
+
+//Function that alerts FrozUNO when a player has one card left
+
+//Function that alerts there's a winner when a player has no cards left
+
+
+
+//LOOPS:
